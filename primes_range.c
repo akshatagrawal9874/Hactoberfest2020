96f38d93495365866f0729c272b168bc79af3821
MANISH SHARMA <56771432+MANISH007700@users.noreply.github.com>
2020-10-05 12:07:30 +0530

    Update primes_range.c

--- primes_range.c
+++ primes_range.c
@@ -1,37 +1,15 @@
-#include <stdio.h>
 
-int main()
-{
-    int number,i,j;
-    printf("Enter the number\n");
-    scanf("%d",&number);
 
-    int primes[number+1];
+ 
 
-    for(i = 2; i<=number; i++)
-        primes[i] = i;
 
-    i = 2;
-    while ((i*i) <= number)
-    {
-        if (primes[i] != 0)
-        {
-            for(j=2; j<number; j++)
-            {
-                if (primes[i]*j > number)
-                    break;
-                else
-                    primes[primes[i]*j]=0;
-            }
-        }
-        i++;
-    }
 
-    for(i = 2; i<=number; i++)
-    {
-        if (primes[i]!=0)
-            printf("%d\n",primes[i]);
-    }
 
-    return 0;
-}
+
+
+
+## Python code to print the prime number till Nth num ! 
+## Please merge this to get those sweet sweet Tees :)
+## This is the drive link to the code
+
+[python-code-using-seiveoferatosthenes](https://drive.google.com/file/d/15MMaga_w6RkdyWM4_j7qHgD10OnhzEEx/view?usp=drivesdk)
